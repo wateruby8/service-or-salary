968b60f0a64e306dbe4bec2a4d539c2f131d2769
water <wateruby8@gmail.com>
2026-01-03 23:18:49 +0800

    GitHub Pages 是靜態主機，需改用 HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 
@@ -10,9 +10,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>,
 )
