a16ffd914a75fafeb108e96c990a8e505e9e3d45
water <wateruby8@gmail.com>
2026-01-09 23:30:35 +0800

    修正bootstrap環境

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 
 import App from './App.jsx'
-
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
