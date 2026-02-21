a84984e19350d82b270f750e332cf7b5e95ff1e1
toku <edie992100@gmail.com>
2026-02-21 17:14:19 +0800

    合併 header 元件

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 
 import Header from "./components/Header";
+import Home from "./pages/Home";
 import { Outlet, NavLink } from "react-router-dom";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <div className="container">
-        {/* 網頁主要區塊 */}
+        <Home />
         {/* <Outlet /> */}
       </div>
-      {/* <Footer /> */}
     </>
   );
 }
