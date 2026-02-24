5578da436f5c070183f3c0a228846ef661a6b7ff
water <wateruby8@gmail.com>
2026-02-24 22:34:58 +0800

    新增footer路徑

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
-import { Outlet, NavLink } from "react-router-dom";
+import Footer from "./components/Footer";
+import {Outlet} from "react-router-dom";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container">
         <Outlet />
       </div>
+      <Footer />      
     </>
   );
 }
