9c8f6ee21a82099a82f8cc10e57977b3b9edc6cc
toku <edie992100@gmail.com>
2026-02-28 15:50:08 +0800

    建立 memberLayout 分支

--- src/pages/MemberInfo.jsx
+++ src/pages/MemberInfo.jsx
@@ -1,8 +1,12 @@
 // 3-1. 接案者-個人專頁
-export default function MemberInfo(){
-    return(
-        <div>
-           接案者-個人專頁
-        </div>
-    )
+import { NavLink } from "react-router-dom";
+
+
+export default function MemberInfo() {
+
+  return (
+    <>
+    
+        </>
+  );
 }
