a2398926dddc4ebd965b414cebec5da0fe6f1f5c
kururu110 <kurururu110@gmail.com>
2026-03-15 19:14:14 +0800

    切版修改

--- src/pages/SellDemo.jsx
+++ src/pages/SellDemo.jsx
@@ -1,30 +1,12 @@
 // 2-1-1. 接案者的公開資料
+import { useState, useEffect } from 'react';
 
 export default function SellDemo(){
     return(<>
-    <div style={{background: "#FBF7F5",minHeight: "100vh",width: "100%",margin: 0,padding: 0}}>
+    <div style={{background: "#FBF7F5",}}>
         <div className="container d-flex gap-7" style={{maxWidth: "1296px", margin: "0 auto"}}>
-            <div className="d-flex container row">
-                <div className="d-flex flex-column align-items-center p-13 rounded-4 col" style={{background: "white"}}>
-                    <h4 className="fw-bold">接案者資訊</h4>
-                    <img src="sellDemo-imgs/FriendlyLin.jpg" className="rounded-circle mb-5" style={{width: "80px", height: "80px"}} alt=""/>
-                    <a className="fs-4 text-decoration-none text-neutral-900 mb-3" href="#">林友善</a>
-                    <p className="fs-6 fw-medium"><img className="me-2" src="sellDemo-imgs/map-icon.svg" alt="地圖圖標" />台北</p>
-                    <ul className="d-flex list-unstyled gap-4 flex-wrap pb-9 mb-9 border-bottom border-1 border-neutral-500">
-                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 攝影</li>
-                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 圖像剪輯</li>
-                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># PhotoShop</li>
-                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 個人照</li>
-                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 台北</li>
-                    </ul>
-                    <ul className="d-flex list-unstyled flex-column lh-1">
-                        <li className="d-flex gap-7"><p>身分認證</p><p className="text-success"><img className="me-2" src="sellDemo-imgs/passed.svg" alt="" />已通過</p></li>
-                        <li className="d-flex gap-7"><p>成交案數</p><p>46</p></li>
-                        <li className="d-flex gap-7"><p>加入時間</p><p>2024/12/6</p></li>
-                        <li className="d-flex gap-7"><p>交易評價</p><p className="d-flex align-items-center">4.7<img className="ms-2" src="sellDemo-imgs/star-fill.svg" alt="星星圖標" /></p></li>
-                    </ul>
-                </div>
-                <div className="col-xxl-9">
+            <div className="d-flex container row align-items-start" style={{ margin: "0 auto"}}>
+                <div className="col-xl-9 order-xl-2">
                     <nav className="py-5" aria-label="breadcrumb">
                         <ol className="breadcrumb">
                             <li className="breadcrumb-item"><a className="text-decoration-none text-neutral-500 fw-medium" href="#">我要委託</a></li>
@@ -38,7 +20,7 @@ export default function SellDemo(){
                             <div className="col-xxl-6">
                                 <img src="sellDemo-imgs/night.jpg" className="rounded-4" style={{width: "100%", maxHeight: "100%"}} alt="" />
                             </div>
-                            <div className="col-xxl-5 p-9 rounded-4" style={{background: "white"}}>
+                            <div className="col-xxl-5 p-9 rounded-4 shadow" style={{background: "white"}}>
                                 <ul className="d-flex list-unstyled gap-3 mb-5">
                                     <li className="tag-shortterm pt-1 pb-1 ps-3 pe-3 rounded-pill">短期</li>
                                     <li className="tag-longterm pt-1 pb-1 ps-3 pe-3 rounded-pill">長期</li>
@@ -68,7 +50,7 @@ export default function SellDemo(){
                         </div>
                         <div>
                         <h3>服務描述</h3>
-                            <div className="p-9 rounded-4" style={{background: "white"}}>
+                            <div className="p-9 rounded-4 shadow" style={{background: "white"}}>
                                 <p>◆服務​特色​​</p>
                                 <p className="mb-0">【客製化​的​拍攝​體驗​】<br />
                                 深度​溝通：​在​拍攝​前，​我​們會​花時​間與​你​們​深入​交流，​了解​你​們​的​愛情​故事、​個性​特點、​喜歡​的​風格​（例如：​唯​美​浪漫、​自然​清新、​復​古​時尚​等），​確保照​片​能​完​美​呈現​你​們​的​「樣子」。<br />
@@ -90,17 +72,35 @@ export default function SellDemo(){
                         </div>
                     </div>
                 </div>
-                
+                <div className="d-flex flex-column align-items-center p-13 rounded-4 col order-xl-1 my-6 shadow" style={{background: "white"}}>
+                    <h4 className="fw-bold">接案者資訊</h4>
+                    <a href=""><img src="sellDemo-imgs/FriendlyLin.jpg" className="rounded-circle mb-5" style={{width: "80px", height: "80px", objectFit: "cover"}} alt="林友善"/></a>
+                    <a className="fs-4 text-decoration-none text-neutral-900 mb-3" href="#">林友善</a>
+                    <p className="fs-6 fw-medium"><img className="me-2" src="sellDemo-imgs/map-icon.svg" alt="地圖圖標" />台北</p>
+                    <ul className="d-flex list-unstyled gap-4 flex-wrap pb-9 mb-9 border-bottom border-1 border-neutral-500">
+                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 攝影</li>
+                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 圖像剪輯</li>
+                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># PhotoShop</li>
+                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 個人照</li>
+                        <li className="bg-primary-100 fs-6 pt-1 pb-1 ps-3 pe-3 rounded-pill"># 台北</li>
+                    </ul>
+                    <ul className="d-flex list-unstyled flex-column lh-1">
+                        <li className="d-flex gap-7"><p>身分認證</p><p className="text-success"><img className="me-2" src="sellDemo-imgs/passed.svg" alt="" />已通過</p></li>
+                        <li className="d-flex gap-7"><p>成交案數</p><p>46</p></li>
+                        <li className="d-flex gap-7"><p>加入時間</p><p>2024/12/6</p></li>
+                        <li className="d-flex gap-7"><p>交易評價</p><p className="d-flex align-items-center">4.7<img className="ms-2" src="sellDemo-imgs/star-fill.svg" alt="星星圖標" /></p></li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <div className="container col row justify-content-end my-14">
+        <div className="container col row justify-content-end my-14" style={{ margin: "0 auto"}}>
             <div className="col-xxl-9">
                 <h3>評價</h3>
                 <div className="mb-5">
-                    <div className="py-7 px-9 rounded-4" style={{background: "white"}}>
+                    <div className="py-7 px-9 rounded-4 mb-5 shadow" style={{background: "white"}}>
                         <div className="d-flex justify-content-between">
                             <div className="d-flex gap-3 mb-7">
-                                <img className="rounded-circle" src="sellDemo-imgs/Martin.jpg" style={{width: "64px", height: "64px"}} alt="" />
+                                <a href="#"><img className="rounded-circle" src="sellDemo-imgs/Martin.jpg" style={{width: "64px", height: "64px", objectFit: "cover"}} alt="Martin" /></a>
                                 <div className="align-self-center">
                                     <a className="fw-medium text-decoration-none text-neutral-900 mt-0" href="#">Martin</a>
                                     <p className="fs-7 mb-0">桃園</p>
@@ -111,10 +111,10 @@ export default function SellDemo(){
                     <p>因​為​另​一​半​非常​嚮往​夢幻​的​場景，​所以​我​們​選擇​了​歐式​宮廷​的​拍攝​風格。​很​感謝​團隊​在​過程​中​循序​漸進​的​引導，​雖然​第一​次​面對​這麼​大量​的​鏡頭，​可是​在​拍攝​時​不​會過於​緊繃​和​生澀，​互動​也​相當​自然，​看完成​品​覺得​非常​滿意！​</p>
                     </div>
                 </div>
-                <div className="py-7 px-9 rounded-4 mb-5" style={{background: "white"}}>
+                <div className="py-7 px-9 rounded-4 mb-5 shadow" style={{background: "white"}}>
                     <div className="d-flex justify-content-between">
                         <div className="d-flex gap-3 mb-7">
-                            <img className="rounded-circle" src="sellDemo-imgs/Alice.jpg" style={{width: "64px", height: "64px"}} alt="" />
+                            <a href="#"><img className="rounded-circle" src="sellDemo-imgs/Alice.jpg" style={{width: "64px", height: "64px", objectFit: "cover"}} alt="Alice" /></a>
                             <div className="align-self-center">
                                 <a className="fw-medium text-decoration-none text-neutral-900 mt-0" href="#">Alice</a>
                                 <p className="fs-7 mb-0">臺北</p>
@@ -128,24 +128,24 @@ export default function SellDemo(){
             </div>
             <div className="col-xxl-9 my-15">
                 <h3>此用戶還有這些服務...</h3>
-                <ul className="list-unstyled d-flex g-5">
-                    <li className="col-lg-4 d-flex flex-column rounded-4 align-items-center" style={{background: "white"}}>
-                    <div className="p-12">
-                        <img src="sellDemo-imgs/graduation.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px"}} alt="" />
+                <ul className="list-unstyled row g-5">
+                    <li className="col-lg-4 d-flex flex-column align-items-center">
+                    <div className="p-12 rounded-4 d-flex flex-column align-items-center shadow" style={{background: "white", width: "100%"}}>
+                        <img src="sellDemo-imgs/graduation.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px", objectFit: "cover"}} alt="" />
                         <h5 className="mb-7 text-center">畢業照拍攝</h5>
                         <button type="button" className="btn btn-secondary-500" style={{width: "160px"}}>前往了解</button> 
                     </div>
                     </li>
-                    <li className="col-lg-4 d-flex rounded-4 flex-column align-items-center" style={{background: "white"}}>
-                    <div className="p-12">
-                        <img src="sellDemo-imgs/pregnant.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px"}} alt="" />
+                    <li className="col-lg-4 d-flex flex-column align-items-center">
+                    <div className="p-12 rounded-4 d-flex flex-column align-items-center shadow" style={{background: "white", width: "100%"}}>
+                        <img src="sellDemo-imgs/pregnant.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px", objectFit: "cover"}} alt="" />
                         <h5 className="mb-7 text-center">孕婦寫真</h5>
                         <button type="button" className="btn btn-secondary-500" style={{width: "160px"}}>前往了解</button> 
                     </div>
                     </li>
-                    <li className="col-lg-4 d-flex rounded-4 flex-column align-items-center" style={{background: "white"}}>
-                        <div className="p-12">
-                            <img src="sellDemo-imgs/company.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px"}} alt="" />
+                    <li className="col-lg-4 d-flex flex-column align-items-center">
+                        <div className="p-12 rounded-4 d-flex flex-column align-items-center shadow" style={{background: "white", width: "100%"}}>
+                            <img src="sellDemo-imgs/company.png" className="mb-7 rounded-4" style={{width: "160px", height: "160px", objectFit: "cover"}} alt="" />
                             <h5 className="mb-7 text-center">企業活動攝影</h5>
                             <button type="button" className="btn btn-secondary-500" style={{width: "160px"}}>前往了解</button> 
                         </div>
