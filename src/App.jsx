eb651faeda22b4daf39c193de392377f280b9a1c
Liloganko <linwhat321485@gmail.com>
2026-03-18 04:27:17 +0800

    修改首頁API參數、優化頁面跳轉

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* 控制頁面變換時，能從頁首開始瀏覽 */}
+      < ScrollToTop/> 
       <Header />
       <Outlet />
       <Footer />      
