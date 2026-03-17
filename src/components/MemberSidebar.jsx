62e11b7e0cca15f41bca0e08da6fa1d05ba9b4d4
toku <edie992100@gmail.com>
2026-03-18 00:04:01 +0800

    調整使用者照片

--- src/components/MemberSidebar.jsx
+++ src/components/MemberSidebar.jsx
@@ -51,7 +51,7 @@ export default function MemberSidebar({ isMobile }) {
         <div className="text-center">
           <div className="member-side__avatar mx-auto mb-5">
             <img
-              src={`${import.meta.env.BASE_URL}memberInfo/user-photo-sm.png`}
+              src={`${import.meta.env.BASE_URL}memberInfo/user-photo-sm.jpg`}
               alt="avatar"
               className="member-side__avatarImg"
             />
