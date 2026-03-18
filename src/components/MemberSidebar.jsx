f5cdd65f48c7341099a3d046c1e9ae8d9ba7dc3d
toku <edie992100@gmail.com>
2026-03-18 21:30:11 +0800

    修正圖片問題(#04)

--- src/components/MemberSidebar.jsx
+++ src/components/MemberSidebar.jsx
@@ -8,6 +8,7 @@ import {
 import { getUserById } from "../api/memberInfoApi";
 
 const CURRENT_USER_ID = import.meta.env.VITE_CURRENT_USER_ID || "u-001";
+const userPhoto = `${import.meta.env.BASE_URL}memberInfo/new-user-photo-sm.jpg`;
 
 export default function MemberSidebar({ isMobile }) {
   const [userName, setUserName] = useState("");
@@ -51,7 +52,7 @@ export default function MemberSidebar({ isMobile }) {
         <div className="text-center">
           <div className="member-side__avatar mx-auto mb-5">
             <img
-              src={`${import.meta.env.BASE_URL}memberInfo/new-user-photo-sm.jpg`}
+              src={userPhoto}
               alt="avatar"
               className="member-side__avatarImg"
             />
